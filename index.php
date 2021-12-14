b9c17a813f0a85be578a408651fe7b04cc39fdfe
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-14 16:36:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -133,7 +133,8 @@
 		$sql = mysqli_query($db, "SELECT `like` FROM `like_cheatnow`;");
 		$res = mysqli_fetch_assoc($sql);
 		$res = $res + 1;
-		$sql2 = mysqli_query($db, "UPDATE `genre` SET `like` = '$res';");
+		
+		$sql2 = mysqli_query($db, "UPDATE `like_cheatnow` SET `like` = '$res';");
 		$res2 = mysqli_fetch_assoc($sql2);
 		echo '<h1>'; echo $res2['like']; echo '</h1>';
 	}
