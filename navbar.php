d126ed2edf6dae57a097187d469a3793f31af2db
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-14 12:26:26 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -51,7 +51,7 @@
 	<header class="header">
 		<div class="navbar">
 			<a href="index.php">HOME</a>
-			<a href="https://www.rapidtables.com/convert/number/decimal-to-binary.html">CONVERT</a>
+			<a href="https://www.rapidtables.com/convert/number/decimal-to-binary.html" target="__blank">CONVERT</a>
 		</div>
 	</header>
 </body>
