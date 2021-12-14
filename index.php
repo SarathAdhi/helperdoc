be2e69cd3201a0c2946fd6732cc45b3ed5c1313e
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-14 13:30:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,9 +23,17 @@
 			<h2>NON RESTORING DIVISION</h2><a href="nondivi.php">click here</a>
 			<h2>BOOTHS MULTIPLICATION</h2><a href="http://www.grahn.us/projects/booths-algorithm/">click here</a>
 			<h2>IEEE CONVERSION</h2><a href="https://binary-system.base-conversion.ro/convert-real-numbers-from-decimal-system-to-32bit-single-precision-IEEE754-binary-floating-point.php">click here</a>
+			<h2>PAGE REPLACEMENT SUM</h2><a href="https://solver.assistedcoding.eu/page_replacement">click here</a>
 		</center>
 	</div>
 </section>
 
 </body>
+<!-- 
+mod 1 perfomance of processors
+mod 2 fully numericals thaan....
+mod 4 memory allocation(fifo,optimal page replacement ,etc....)
+mod 6 raid
+thats all ig..... 
+-->
 </html>
