f304925b3b194ef70a485166743726ee6529cf57
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-14 17:33:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -135,7 +135,7 @@
 
 		while($row = mysqli_fetch_assoc($check))
 		{
-			if($row['name'] != $_POST['user'] && $row['ip'] != $ip)
+			if($row['name'] != $_POST['name'] && $row['ip'] != $ip)
 			{
 				$sql = mysqli_query($db, "INSERT INTO `user_cheatnow` VALUES ('$_POST[name]', '$ip');");
 
