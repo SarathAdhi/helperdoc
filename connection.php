7bda6fc9aadc37950ad0504b403cd3458b3e8812
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-14 16:04:42 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+
+	$db = mysqli_connect("remotemysql.com","rILqN3VfQi","uFB7I2wk5r","rILqN3VfQi"); 	/* server name, username, password, database name */
+?>
+
+<?php
+/* this one not needed below thing*/
+/*
+if(!$db)	
+{
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully"
+
+*/
+?>
