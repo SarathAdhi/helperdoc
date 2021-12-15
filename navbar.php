ba1820dbfb70edc6a2ab48129581ae5ea95b4d38
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-15 20:40:35 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -79,25 +79,25 @@
 		</div>
 
 		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Or Sharabi</a>
-    </div>
-    <div class="navbar-collapse collapse">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="active"><a href="#about">About</a></li>
-        <li class="active"><a href="#portfolio">Portfolio</a></li>
-        <li class="active"><a href="#contact">Contact</a></li>
-      </ul>
-    </div>
-  </div>
+		<div class="navbar">
+		    <div class="navbar-header">
+		      	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+		      	</button>
+		      	<a class="navbar-brand" href="#">Or Sharabi</a>
+		    </div>
+		    <div class="navbar-collapse collapse">
+		      	<ul class="nav navbar-nav navbar-right">
+			        <li class="active"><a href="#">Home</a></li>
+			        <li class="active"><a href="#about">About</a></li>
+			        <li class="active"><a href="#portfolio">Portfolio</a></li>
+		        	<li class="active"><a href="#contact">Contact</a></li>
+		      	</ul>
+		    </div>
+		</div>
 </div>
 	</header>
 </body>
