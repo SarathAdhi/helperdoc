bd239f0debe2af6717155ee5d2537b29a722ec29
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-15 06:26:24 +0530

    Update adminpage.php

---
+++ adminpage.php
@@ -0,0 +1,76 @@
+<?php
+	include "navbar.php";
+	include "connection.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HOME</title>
+	<style type="text/css">
+		.content
+		{
+
+		}
+		.content .border
+		{
+			border: 5px solid red;
+		}
+		.content .border2
+		{
+			border: 5px solid blue;
+		}
+		.scroll 
+		{
+			position: fixed;
+			top: 90%;
+			right: 0;
+			text-decoration: none;
+			background-color: lightgrey;
+			width: 30px;
+		}
+		.scroll span
+		{
+			font-size: 30px;
+			color: black;
+		}
+
+	</style>
+</head>
+<body>
+
+<section>
+
+	<a class="scroll" href="#content"><span>&#11165;</span></a>
+
+	<div class="content" id="content">
+		<center>
+
+			<div class="border">
+
+				<h2>Flynn’s Taxonomy & Classification</h2>
+				<a href="image/ft1.png" >Flynn’s Taxonomy</a>
+				<a href="image/ft2.png" >Flynn’s Classification</a>
+				<a href="image/ft3.png" >types-> SISD</a>
+				<a href="image/ft4.png" >types-> SIMD</a>
+				<a href="image/ft5.png" >SIMD diagram</a>
+				<a href="image/ft6.png" >types-> MISD</a>
+				<a href="image/ft7.png" >MISD diagram</a>
+				<a href="image/ft8.png" >types-> MIMD</a>
+				<a href="image/ft9.png" >MIMD diagram</a>
+				<br><br>
+			</div>
+
+			<br><br>
+
+			
+		</center>
+	</div>
+</section>
+
+<h2>தூய தமிழன்</h2>
+
+</body>
+</html>
