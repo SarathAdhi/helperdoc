c5c7ee60a79ac891b573c02c2c9e46b34c85b3f7
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-15 06:15:09 +0530

    Update admin.php

---
+++ admin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>admin</title>
+</head>
+<body>
+
+<section>
+	<div class="content">
+
+		<form action="" method="post">
+			
+			<p style="font-size: 20px;">password:</p>
+			<input type="password" name="password" placeholder="Enter password" required width="300px" height="10px">
+			<button type="submit" name="submit"></button>
+
+		</form>
+
+	</div>
+</section>
+
+<?php
+	if(isset($_POST['submit']))
+	{
+		$pass = 123123123;
+		if($_POST['password'] == $pass)
+		{
+		?>
+			<script type="text/javascript">
+				location.replace('adminpage.php');
+			</script>
+		<?php
+		}
+		else
+		{
+		?>
+			<script type="text/javascript">
+				alert("Wrong password :(");
+			</script>
+		<?php
+		}
+
+	}
+?>
+
+</body>
+</html>
