7b5a61aecdd35309ce391f0a8e8b8b0d266e56e7
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-15 06:07:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
 			<div class="border">
 			
 				<h2>RESTORING DIVISION</h2>
-				<a href="5.png" target="_blank">view flowchart</a>
+				<a href="image/5.png" target="_blank">view flowchart</a>
 				<br><br>
 
 			</div>
@@ -118,7 +118,7 @@
 			
 				<h2>NON RESTORING DIVISION</h2><a href="nondivi.php" >click here</a>
 				<br>
-				<a href="4.png" target="_blank">view flowchart</a>
+				<a href="image/4.png" target="_blank">view flowchart</a>
 				<br><br>
 
 			</div>
@@ -182,7 +182,7 @@
 
 			<div class="border2">
 
-				<h2>ADDITION & SUBTRACTION</h2><a href="3.jpg" target="_blank">view flowchart</a>
+				<h2>ADDITION & SUBTRACTION</h2><a href="image/3.jpg" target="_blank">view flowchart</a>
 				<br><br>
 
 			</div>
