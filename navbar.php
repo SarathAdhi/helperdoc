c0e4559741f061281bc8555d0f92c9ce3156a95f
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-15 20:47:13 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -32,7 +32,7 @@
 			border-radius: 10px;
 			z-index: 1000;
 		}
-		.navbar
+		.header .navbar
 		{
 			padding-top: 10px;
 			justify-content: center;
@@ -64,33 +64,19 @@
 </head>
 <body>
 	<header class="header">
-		
-
 		<div class="navbar">
-			<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-				<div class="container">
-				    <div class="navbar-header">
-				      	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					        <span class="sr-only">Toggle navigation</span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-				      	</button>
-				    </div>
-
-				    <div class="navbar-collapse collapse">
-				      	<ul class="nav navbar-nav navbar-right">
-					        <li class="active"><a href="#">Home</a></li>
-					        <li class="active"><a href="#about">About</a></li>
-					        <li class="active"><a href="#portfolio">Portfolio</a></li>
-				        	<li class="active"><a href="#contact">Contact</a></li>
-				      	</ul>
-				    </div>
-				</div>
-			</div>
+			<a href="index.php">HOME</a>
+			<a href="index.php">COURSES</a>
+			<a href="https://www.rapidtables.com/convert/number/decimal-to-binary.html" target="__blank">CALCULATOR</a>
+			<?php
+				if(isset($_SESSION['username']))
+				{
+				?>
+					<a href="logout.php" class="logout" style="text-decoration: none;">Logout<i class="fa fa-sign-out"></i></a>
+				<?php
+				}
+			?>
 		</div>
 	</header>
 </body>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
 </html>
