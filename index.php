2d483db55faa98c491d827f2edd41fca51d92143
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-16 08:45:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -251,5 +251,6 @@
         $_SESSION['username'] = $_POST['username'];
     }
 ?>
-
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
 </html>
