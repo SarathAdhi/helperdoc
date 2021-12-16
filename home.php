07b7e5afcd1d7b4e564ebf5425b19b6e7a42e30a
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-16 08:57:33 +0530

    Update home.php

--- home.php
+++ home.php
@@ -16,92 +16,7 @@
             font-family: Consolas !important;
         }
 
-        h2,p
-        {
-            font-family: Consolas !important;
-        }
-
-        *
-        {
-            box-sizing: border-box;
-        }
-
-        .open-button
-        {
-            background-color: #555;
-            color: white;
-            padding: 16px 20px;
-            border: none;
-            cursor: pointer;
-            position: relative;
-            width: 150px;
-        }
-
-
-        .form-popup
-        {
-            display: none;
-            position: relative;
-            border: 3px solid #0A0162;
-            z-index: 9;
-            max-width: 600px;
-            border-radius: 10px;
-        }
-
-        .form-container
-        {
-            width: 99%;
-            padding: 30px;
-            background-color: white;
-            border-radius: 10px;
-
-        }
-
-        .form-popup span
-        {
-            position: absolute;
-            margin-top: -25px;
-            font-size: 30px;
-            color: #0A0162;
-        }
-
-        .form-container input
-        {
-            width: 100%;
-            padding: 15px;
-            border: none;
-            background: #f1f1f1;
-            border-radius: 10px;
-        }
-
-
-
-        .form-container .btn
-        {
-            background-color: #04AA6D;
-            color: white;
-            padding: 16px 20px;
-            border: none;
-            cursor: pointer;
-            width: 50%;
-            margin-bottom: 10px;
-            opacity: 0.8;
-            border-radius: 10px;
-        }
-
-
-        .form-container .cancel
-        {
-            background-color: red;
-        }
-
-
-        .form-container .btn:hover,
-        .open-button:hover
-        {
-            opacity: 1;
-        }
-        .typing-demo h1{
+        h1{
           animation: type 3s steps(30);
           color:lightgreen;
           white-space:nowrap;
@@ -125,7 +40,7 @@
           100%{opacity:1;}
         }
 
-        .typing-demo blink{
+        blink{
           animation: blink 1s linear infinite;
           border-right: 10px solid lightgreen;
           height:12px;
@@ -138,7 +53,9 @@
         <?php
             if(isset($_SESSION['username']))
             {
-                echo '<h1 class="typing-demo">Hii '.$_SESSION['username'].', Welcome to HelperDOC.<blink></blink></h1>';
+                ?>
+                <h1>Hii <?php $_SESSION['username'] ?>, Welcome to HelperDOC.<blink></blink></h1>
+                <?php
             }
             else
             {
