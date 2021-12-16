b2c88da329a1c63235cf46dfa08d045e92c031cb
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-16 14:12:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -210,7 +210,7 @@
                         $hash1 = password_hash($pass1, PASSWORD_DEFAULT);
                         echo "Entered Text:  ".$pass1."\n";
                         echo '<br>';
-                        echo "Hashed Text:  ".$hash1."\n";
+                        echo "Hashed Code:  ".$hash1."\n";
                     }
                 ?>
             </div>
