4dec578db7af744ff0ddbbead8b1311a5cd54660
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-17 15:19:54 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,195 +13,222 @@
     />
 
     <style type="text/css">
-    	::-webkit-scrollbar {
+    	::-webkit-scrollbar
+    	{
 		  width: 1px;
 		}
-.body
-{
-	margin-top: 5px;
-  display: grid;
-  place-items: center;
-}
-.header {
-  background-color: #523BF3;
-  font-family: Consolas !important;
-  width: 70%;
-  border-radius: 10px;
-}
 
-.main-nav {
-  height: 90px;
-}
+		.body
+		{
+			margin-top: 5px;
+		  	display: grid;
+		  	place-items: center;
+		}
 
+		.header
+		{
+		  	background-color: #523BF3;
+		  	font-family: Consolas !important;
+		  	width: 70%;
+		  	border-radius: 10px;
+		}
 
-.logo {
-  color: palegoldenrod;
-  line-height: 90px;
-  font-size: 30px;
-  font-weight: bold;
-  text-decoration: none;
-  margin-left: 30px;
-  font-family: "AstroSpace", sans-serif;
-}
-.logo:hover {
-	color: white;
-  text-decoration: none;
-}
-.text
-{
-  color: white;
-  line-height: 90px;
-  font-size: 20px;
-  font-weight: bold;
-  text-decoration: none;
-  margin-left: 10px;
-  font-family: "AstroSpace", sans-serif;
-  display: none;
-}
+		.main-nav
+		{
+		  	height: 90px;
+		}
 
-.main-nav .logoimg
-{
-  position: absolute;
-  height: 40px;
-  margin-top: 25px;
-  padding-left: 10px;
-}
+		.logo
+		{
+		  	color: palegoldenrod;
+		  	line-height: 90px;
+		  	font-size: 30px;
+		  	font-weight: bold;
+		  	text-decoration: none;
+		  	margin-left: 30px;
+		  	font-family: "AstroSpace", sans-serif;
+		}
 
-.navlinks {
-  list-style: none;
-  float: right;
-  line-height: 90px;
-  margin: 0;
-  padding: 0;
-}
+		.logo:hover
+		{
+			color: white;
+		  	text-decoration: none;
+		}
+		.text
+		{
+		  	color: white;
+		  	line-height: 90px;
+		  	font-size: 20px;
+		  	font-weight: bold;
+		  	text-decoration: none;
+		  	margin-left: 10px;
+		  	font-family: "AstroSpace", sans-serif;
+		  	display: none;
+		}
 
-.navlinks li {
-  display: inline-block;
-  margin: 0 20px;
-}
+		.main-nav .logoimg
+		{
+		  	position: absolute;
+		  	height: 40px;
+		 	margin-top: 25px;
+		  	padding-left: 10px;
+		}
 
-.navlinks li a {
-  color: white;
-  text-decoration: none;
-  font-size: 18px;
-  text-transform: uppercase;
-  transition: all 0.3s linear 0s;
-  font-weight: bold;
-}
+		.navlinks
+		{
+		  	list-style: none;
+		  	float: right;
+		  	line-height: 90px;
+		  	margin: 0;
+		  	padding: 0;
+		}
 
-.navlinks li a:hover {
-  color: #7ebcb9;
-  padding-bottom: 7px;
-  border-bottom: 2px solid #7ebcb9;
-}
+		.navlinks li
+		{
+		  	display: inline-block;
+		  	margin: 0 20px;
+		}
 
+		.navlinks li a
+		{
+		  	color: white;
+		  	text-decoration: none;
+		  	font-size: 18px;
+		  	text-transform: uppercase;
+		  	transition: all 0.3s linear 0s;
+		  	font-weight: bold;
+		}
 
-.menu-btn {
-  color: white;
-  font-size: 25px;
-  float: right;
-  line-height: 90px;
-  margin-right: 40px;
-  cursor: pointer;
-  display: none;
-}
-
-#isChecked {
-  display: none;
-}
-
-@media (max-width: 800px) {
-  .navlinks {
-    position: fixed;
-    width: 100%;
-    height: 40vh;
-    text-align: center;
-    background: #222831;
-    top: -100%;
-    transition: all 0.5s;
-    border-radius: 10px;
-    z-index: 9;
-  }
-
-  .navlinks li {
-    display: block;
-  }
-
-  .navlinks li a {
-    font-size: 20px;
-  }
-
-  .navlinks li a:hover {
-    border-bottom: none;
-  }
-  .menu-btn {
-    display: block;
-  }
-  .logo {
-    display: none;
-  }
-  .text
-  {
-    display: inline-block;
-  }
-
-  #isChecked:checked ~ .navlinks {
-    top: 15%;
-  }
-}
-
-@media (max-width: 800px) {
-
-  .header
-  {
-    width: 100%;
-  }
-  .checkbox
-  {
-    background-color: white;
-  }
-  
-  .logoimg
-  {
-
-    display: inline-block;
-  }
-
-  .menu-btn {
-    margin-right: 15px;
-    font-size: 25px;
-  }
-
-}
+		.navlinks li a:hover
+		{
+		  	color: #7ebcb9;
+		  	padding-bottom: 7px;
+		  	border-bottom: 2px solid #7ebcb9;
+		}
 
 
+		.menu-btn
+		{
+		  	color: white;
+		  	font-size: 25px;
+		  	float: right;
+		  	line-height: 90px;
+		  	margin-right: 40px;
+		  	cursor: pointer;
+		  	display: none;
+		}
+
+		#isChecked
+		{
+		  	display: none;
+		}
+
+		@media (max-width: 800px)
+		{
+		  .navlinks
+		  {
+		    position: fixed;
+		    width: 100%;
+		    height: 40vh;
+		    text-align: center;
+		    background: #222831;
+		    top: -100%;
+		    transition: all 0.5s;
+		    border-radius: 10px;
+		    z-index: 9;
+		  }
+
+		  .navlinks li
+		  {
+		    display: block;
+		  }
+
+		  .navlinks li a
+		  {
+		    font-size: 20px;
+		  }
+
+		  .navlinks li a:hover
+		  {
+		    border-bottom: none;
+		  }
+
+		  .menu-btn
+		  {
+		    display: block;
+		  }
+
+		  .logo
+		  {
+		    display: none;
+		  }
+
+		  .text
+		  {
+		    display: inline-block;
+		  }
+
+		  #isChecked:checked ~ .navlinks
+		  {
+		    top: 15%;
+		  }
+		}
+
+		@media (max-width: 800px)
+		{
+		  .header
+		  {
+		    width: 100%;
+		  }
+
+		  .checkbox
+		  {
+		    background-color: white;
+		  }
+		  
+		  .logoimg
+		  {
+
+		    display: inline-block;
+		  }
+
+		  .menu-btn {
+		    margin-right: 15px;
+		    font-size: 25px;
+		  }
+		}
     </style>
-  </head>
-  <div class="body">
-    <header class="header">
-      <nav class="main-nav">
-        <input type="checkbox" id="isChecked" />
-        <label for="isChecked" class="menu-btn">
-          <i class="fas fa-bars"></i>
-        </label>
-        <a href="index.php" class="logo">HelperDOC </a>
-        <a href="index.php" class="text">H_DOC</a>
-        <a href="index.php"><img src="background/open-book.png" class="logoimg"></a>
-        <ul class="navlinks">
-        	<li><a href="index.php">HOME</a></li>
-			<li><a href="courses.php">COURSES</a></li>
-          	<li><a href="#">ABOUT</a></li>
-          	<?php
-          	if(isset($_SESSION['username']))
-          	{
-          	?>
-				<li><a href="logout.php" style="text-decoration: none;">< <?php echo $_SESSION['username']; ?> ><i class="fa fa-sign-out"></i></a></li>
-			<?php
-			}
-			?>
+ </head>
 
-        </ul>
-      </nav>
+ <div class="body">
+    <header class="header">
+      	<nav class="main-nav">
+	        <input type="checkbox" id="isChecked" />
+
+	        <label for="isChecked" class="menu-btn">
+	        	<i class="fas fa-bars"></i>
+	        </label>
+
+	        <a href="index.php" class="logo">HelperDOC </a>
+	        <a href="index.php" class="text">H_DOC</a>
+	        <a href="index.php"><img src="background/open-book.png" class="logoimg"></a>
+
+	        <ul class="navlinks">
+
+	        	<li><a href="index.php">HOME</a></li>
+				<li><a href="courses.php">COURSES</a></li>
+	          	<li><a href="#">ABOUT</a></li>
+	          	<?php
+	          	if(isset($_SESSION['username']))
+	          	{
+	          	?>
+					<li><a href="logout.php" style="text-decoration: none;">< <?php echo $_SESSION['username']; ?> ><i class="fa fa-sign-out"></i></a></li>
+				<?php
+				}
+				?>
+
+	        </ul>
+      	</nav>
     </header>
-  </div>
+</div>
 </html>
