e427b3efc3966f11801c16757eecc916d637d55d
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-17 13:14:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -126,12 +126,12 @@
 
                 <form action="" method="post" class="form-container">
                     <h1>Login</h1>
-                    <br><br>
+                    <br>
 
-                    <label for="username"><b>Username</b></label>
+                    <label for="username"><b>Username</b></label><br>
                     <input type="text" placeholder="Enter Your Username" name="username" required>
                     <br><br>
-                    <label for="password"><b>Password</b></label>
+                    <label for="password"><b>Password</b></label><br>
                     <input type="password" placeholder="Enter Password" name="password" required>
                     <br><br>
                     <button type="submit" class="btn" name="loginsubmit">Login</button>
@@ -146,18 +146,29 @@
 
                 <form action="" method="post" class="form-container">
                     <h1>Signup</h1>
+                    <br>
 
-                    <label for="username"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="username" required>
+                    <label for="username"><b>Username</b></label><br>
+                    <input type="text" placeholder="Enter Your Username" name="username" required>
                     <br><br>
-                    <label for="password"><b>Password</b></label>
+                    <label for="password"><b>Password</b></label><br>
                     <input type="password" placeholder="Enter Password" name="password" required>
                     <br><br>
                     <label for="degree"><b>Degree</b></label>
-                    <input type="text" placeholder="B.tech, M.tech, ..." name="degree" required>
+                    <select name="degree" size="1" style="color: black; font-size: 20px">
+                        <option value="B.Tech">B.Tech</option>
+                        <option value="M.Tech">M.Tech</option>
+                        <option value="other">other</option>
+                    </select>
                     <br><br>
                     <label for="year"><b>Graduate Year</b></label>
-                    <input type="text" placeholder="2021, 2022, ..." name="year" required>
+                    <select name="year" size="1" style="color: black; font-size: 20px">
+                        <option value="2022">2022</option>
+                        <option value="2023">2023</option>
+                        <option value="2024">2024</option>
+                        <option value="2025">2025</option>
+                        <option value="2026">2026</option>
+                    </select>
                     <br><br>
                     <button type="submit" class="btn" name="register">Register</button>
                     <br>
