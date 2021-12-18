9fd8fad70d22dd6bd7065357600fcb6bfd3c10e3
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-18 08:15:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -178,7 +178,7 @@
         $sql2 = mysqli_query($db, "INSERT INTO `notes_hd` VALUES ('$_POST[coursecode]', '$_POST[tl]', '$_POST[module]', '$_POST[link]');");
         ?>
         <script type="text/javascript">
-            alert("Successfully Uploaded created.");
+            alert("Successfully Uploaded.");
         </script>
 <?php
     }
