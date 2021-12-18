8d6130eded4f67165b5aa5da9e9ce8f7d2153a02
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-18 08:46:26 +0530

    Update notesview.php

--- notesview.php
+++ notesview.php
@@ -4,6 +4,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $_GET['id'] ?></title>
+
+	<style type="text/css">
+		body
+		{
+            background-image: url('background/1.jpg');
+            font-family: Consolas !important;
+		}
+	</style>
 </head>
 <body>
 	<section class="section1">
