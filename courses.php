71c94a3d226d48582af62ded4d42b77a801a6f7e
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-18 18:02:22 +0530

    Update courses.php

--- courses.php
+++ courses.php
@@ -127,12 +127,14 @@
 		<?php
 			$sql = mysqli_query($db, "SELECT DISTINCT `coursecode` FROM `notes_hd`;");
 
-			echo '<div class="content" id="content">';
+			
 			while($row = mysqli_fetch_assoc($sql))
 			{
+				echo '<div class="content" id="content">';
 				echo '<h3>'.$row['coursecode'].' <br><a href="notesview.php?id='.$row['coursecode'].'" class="btn btn-success">click here</a></h3>';
+				echo '</div>';
 			}
-			echo '</div><br>';
+			
 
 		?>
 		
