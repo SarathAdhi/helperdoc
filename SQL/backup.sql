8351f1174c43984c92c5176dc538ae936d320306
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-19 07:12:33 +0530

    Update backup.sql

--- SQL/backup.sql
+++ SQL/backup.sql
@@ -1,7 +1,21 @@
 drop table user_cheatnow;
+
 CREATE TABLE `user_cheatnow` (
 `username` text(20) NOT NULL,
   `password` text NOT NULL,
   `dept` text NOT NULL,
   primary key(username(30))
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE user_hd
+ADD COLUMN count integer; 
+
+ALTER TABLE notes_hd
+MODIFY COLUMN module text;  
+
+CREATE TABLE `notes_hd` (
+`coursecode` text NOT NULL,
+  `tl` text NOT NULL,
+  `module` int NOT NULL,
+  `link` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
