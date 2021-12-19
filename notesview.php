781c5d567bbea444853133f01e0fbe2d91a07ee8
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-19 08:51:27 +0530

    Update notesview.php

--- notesview.php
+++ notesview.php
@@ -39,7 +39,7 @@
 
 				while($row = mysqli_fetch_assoc($sql))
 				{
-					if($row['module'] == 'Syllabus')
+					if($row['module'] == 'Syllabus' || $row['topic'] == 'Syllabus')
 					{
 						if($count == 0)
 						{
