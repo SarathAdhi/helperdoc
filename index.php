74306e695d8d3b249ac368522a6cee069c8764d0
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-23 16:23:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -191,19 +191,10 @@
 <?php
     if(isset($_POST['upload']))
     {
-        if($sql2 = mysqli_query($db, "INSERT INTO `notes_hd` VALUES ('$_POST[coursecode]', '$_POST[tl]', '$_POST[module]', '$_POST[link]', '$_POST[cmts]');"))
-        {
+        $sql2 = mysqli_query($db, "INSERT INTO `notes_hd` VALUES ('$_POST[coursecode]', '$_POST[tl]', '$_POST[module]', '$_POST[link]', '$_POST[cmts]');");
         ?>
         <script type="text/javascript">
             alert("Successfully Uploaded :)");
-        </script>
-        <?php
-        }
-        else
-        {
-        ?>
-        <script type="text/javascript">
-            alert("Not Uploaded Successfully :(");
         </script>
         <?php
         }
