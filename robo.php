a7e7e73cabcf72f8e994983f8be6a3dbf80290a2
SarathAdhi <s.sarathadhithya@gmail.com>
2021-12-27 11:41:11 +0530

    Create robo.php

---
+++ robo.php
@@ -0,0 +1,30 @@
+<?php
+	include "navbar.php";
+	include "connection.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>HDoc | COURSES</title>
+	<style type="text/css">
+		body
+		{
+			background-color: #EBEAFD;
+			font-family: Consolas !important;
+		}
+
+	</style>
+</head>
+<body>
+
+<section>
+	<iframe src="https://www.jdoodle.com/iembed/v0/ls5" style="border:0px #ffffff none;" name="myiFrame" frameborder="1" marginheight="0px" marginwidth="0px" height="800px" width="600px" allowfullscreen></iframe>
+</section>
+
+</body>
+</html>
