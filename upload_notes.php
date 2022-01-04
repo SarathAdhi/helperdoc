8d1101a6b2c2ca17d3e06869e80e46d99ca55577
SarathAdhi <s.sarathadhithya@gmail.com>
2022-01-04 09:36:08 +0530

    Update upload_notes.php

--- upload_notes.php
+++ upload_notes.php
@@ -48,7 +48,8 @@
 <?php
     if(isset($_POST['upload']))
     {
-        $sql2 = mysqli_query($db, "INSERT INTO `notes_hd` VALUES ('$_POST[coursecode]', '$_POST[tl]', '$_POST[module]', '$_POST[link]', '$_POST[cmts]');");
+        $coursecode = strtoupper($_POST['coursecode']);
+        $sql2 = mysqli_query($db, "INSERT INTO `notes_hd` VALUES ('$coursecode', '$_POST[tl]', '$_POST[module]', '$_POST[link]', '$_POST[cmts]');");
         ?>
         <script type="text/javascript">
             alert("Successfully Uploaded :)");
