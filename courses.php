40afb90a3fb1a4414a036b6fb367fb7d64596324
SarathAdhi <s.sarathadhithya@gmail.com>
2022-01-04 10:58:00 +0530

    Update courses.php

--- courses.php
+++ courses.php
@@ -19,14 +19,15 @@
 			font-family: Consolas !important;
 		}
 		.courses{
-			display: flex;
-			flex: 1;
+			width:  100%;
+			height: 100%;
+			position: relative;
+			justify-content: center;
+			text-align: center;
 		}
 		.content
 		{
-			display: flex;
-			flex: 1;
-			justify-content: space-between;
+			display: inline-block;
 			width: 200px;
 			height: 200px;
 			padding: 20px;
