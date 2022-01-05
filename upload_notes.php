aa7e66ea5dcf804769264e565bfee4e132a86952
SarathAdhi <s.sarathadhithya@gmail.com>
2022-01-05 15:14:13 +0530

    Update upload_notes.php

--- upload_notes.php
+++ upload_notes.php
@@ -9,7 +9,7 @@
 
         <label for="tl"><b>Theory or Lab&emsp;</b></label>
         <select name="tl" size="1" style="color: black; font-size: 20px">
-            <option value="Theroy">Theory</option>
+            <option value="Theory">Theory</option>
             <option value="Lab">Lab</option>
         </select>
         <br><br>
