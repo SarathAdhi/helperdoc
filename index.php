f6f9155cfdf2e0f3077bb1ed9db7325df75f0d98
SarathAdhi <s.sarathadhithya@gmail.com>
2022-01-31 08:31:53 +0530

    Ads

--- index.php
+++ index.php
@@ -167,6 +167,17 @@ else
 
             <br><br>
         </center>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8031548643936718" crossorigin="anonymous"></script>
+        <!-- HelperDOC -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-8031548643936718"
+            data-ad-slot="2713127391"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
     </section>
 <?php
     }
