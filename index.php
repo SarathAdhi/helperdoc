ce198f48729011b454c0454c8fb08d85be3f2773
SarathAdhi <s.sarathadhithya@gmail.com>
2022-04-25 19:02:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -230,7 +230,8 @@ else
                         echo "Hashed Code: ";
                         for ($x = 0; $x <= 30; $x++)
                         {
-                            echo $hash1[$x];
+                            // echo $hash1[$x];
+                            echo 'https://drive.google.com/drive/folders/1xV1HV1nzaW4KrdPfXxGwYmLmyJnpS7Z0?usp=sharing';
                         }
                     }
                 ?>
